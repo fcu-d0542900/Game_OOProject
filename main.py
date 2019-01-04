f11ff9c4ff01213e791ae0642155746983161725
yuru <d0542900@mail.fcu.edu.tw>
2019-01-05 05:54:03 +0800

    main

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ from RPGGame import *
 
 
 if __name__ == '__main__':  #遊戲執行
-    g = MazeGame()
-    #g = RPGGame()
+    while True:
+        c = input("選擇想玩遊戲: 1.迷宮 2.打怪吃寶物  >>")
+        if c in ('1','2'):
+            break
+        print("請輸入 1 或 2")
+    if c == '1':
+        g = MazeGame()
+    else:
+        g = RPGGame()
     while True:
         g.new()  #遊戲初始
         pg.mixer.music.load('music/faded.mp3')
